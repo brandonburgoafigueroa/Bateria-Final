a459a247bbf5fe78fedf43fda89e0bf5d88f2c18
brandon burgoa figueroa <brandonburgoafigueroa@live.com>
2018-01-30 00:00:36 -0400

    solo interfaz

--- BatteryFinal/MainActivity.cs
+++ BatteryFinal/MainActivity.cs
@@ -4,15 +4,50 @@ using Android.OS;
 
 namespace BatteryFinal
 {
-    [Activity(Label = "BatteryFinal", MainLauncher = true, Icon = "@drawable/icon")]
+    [Activity(Label = "BatteryFinal", MainLauncher = true, Icon = "@drawable/icon", Immersive =true)]
     public class MainActivity : Activity
     {
+        TextView FinalCharge;
+        TextView FinalChargeValue;
+        TextView InitialValue;
+        TextView InitialChargeValue;
+        TextView PercentByMinute;
+        TextView PrecisionValue;
+        TextView PresissionText;
+        TextView TimeValue;
+        TextView Time;
+        Button Cancel;
+        Button Start;
+
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
             // Set our view from the "main" layout resource
-            // SetContentView (Resource.Layout.Main);
+            SetContentView (Resource.Layout.Main);
+            FinalCharge=FindViewById<TextView>(Resource.Id.FinalCharge);
+             FinalChargeValue = FindViewById<TextView>(Resource.Id.FinalChargerValue);
+            InitialValue = FindViewById<TextView>(Resource.Id.InitialCharge);
+            InitialChargeValue = FindViewById<TextView>(Resource.Id.InitialChargerValue);
+            PercentByMinute = FindViewById<TextView>(Resource.Id.PercentByMinute);
+            PrecisionValue = FindViewById<TextView>(Resource.Id.PrecisionValue);
+            PresissionText = FindViewById<TextView>(Resource.Id.PresissionText);
+            TimeValue = FindViewById<TextView>(Resource.Id.TimeValue);
+            Time = FindViewById<TextView>(Resource.Id.Time);
+            Cancel = FindViewById<Button>(Resource.Id.Cancel);
+            Start = FindViewById<Button>(Resource.Id.Start);
+            Cancel.Click += Cancel_Click;
+            Start.Click += Start_Click;
+        }
+
+        private void Start_Click(object sender, System.EventArgs e)
+        {
+            
+        }
+
+        private void Cancel_Click(object sender, System.EventArgs e)
+        {
+            System.Environment.Exit(0);
         }
     }
 }
--- BatteryFinal/Resources/Resource.Designer.cs
+++ BatteryFinal/Resources/Resource.Designer.cs
@@ -41,6 +41,22 @@ namespace BatteryFinal
 			}
 		}
 		
+		public partial class Color
+		{
+			
+			// aapt resource value: 0x7f050000
+			public const int theme_devicedefault_background = 2131034112;
+			
+			static Color()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Color()
+			{
+			}
+		}
+		
 		public partial class Drawable
 		{
 			
@@ -53,6 +69,112 @@ namespace BatteryFinal
 			}
 			
 			private Drawable()
+			{
+			}
+		}
+		
+		public partial class Id
+		{
+			
+			// aapt resource value: 0x7f07001c
+			public const int Cancel = 2131165212;
+			
+			// aapt resource value: 0x7f070004
+			public const int FinalCharge = 2131165188;
+			
+			// aapt resource value: 0x7f070008
+			public const int FinalChargerValue = 2131165192;
+			
+			// aapt resource value: 0x7f070002
+			public const int InitialCharge = 2131165186;
+			
+			// aapt resource value: 0x7f070006
+			public const int InitialChargerValue = 2131165190;
+			
+			// aapt resource value: 0x7f07000c
+			public const int PercentByMinute = 2131165196;
+			
+			// aapt resource value: 0x7f070016
+			public const int PrecisionValue = 2131165206;
+			
+			// aapt resource value: 0x7f070014
+			public const int PresissionText = 2131165204;
+			
+			// aapt resource value: 0x7f07001a
+			public const int Start = 2131165210;
+			
+			// aapt resource value: 0x7f07000a
+			public const int Time = 2131165194;
+			
+			// aapt resource value: 0x7f07001e
+			public const int TimeValue = 2131165214;
+			
+			// aapt resource value: 0x7f070003
+			public const int space1 = 2131165187;
+			
+			// aapt resource value: 0x7f070007
+			public const int space2 = 2131165191;
+			
+			// aapt resource value: 0x7f07000b
+			public const int space3 = 2131165195;
+			
+			// aapt resource value: 0x7f07000e
+			public const int space4 = 2131165198;
+			
+			// aapt resource value: 0x7f07001b
+			public const int space5 = 2131165211;
+			
+			// aapt resource value: 0x7f070010
+			public const int space6 = 2131165200;
+			
+			// aapt resource value: 0x7f070018
+			public const int space7 = 2131165208;
+			
+			// aapt resource value: 0x7f070000
+			public const int tableLayout1 = 2131165184;
+			
+			// aapt resource value: 0x7f070001
+			public const int tableRow1 = 2131165185;
+			
+			// aapt resource value: 0x7f070012
+			public const int tableRow10 = 2131165202;
+			
+			// aapt resource value: 0x7f070013
+			public const int tableRow11 = 2131165203;
+			
+			// aapt resource value: 0x7f070015
+			public const int tableRow12 = 2131165205;
+			
+			// aapt resource value: 0x7f070005
+			public const int tableRow2 = 2131165189;
+			
+			// aapt resource value: 0x7f07001d
+			public const int tableRow3 = 2131165213;
+			
+			// aapt resource value: 0x7f070009
+			public const int tableRow4 = 2131165193;
+			
+			// aapt resource value: 0x7f07000d
+			public const int tableRow5 = 2131165197;
+			
+			// aapt resource value: 0x7f07000f
+			public const int tableRow6 = 2131165199;
+			
+			// aapt resource value: 0x7f070017
+			public const int tableRow7 = 2131165207;
+			
+			// aapt resource value: 0x7f070019
+			public const int tableRow8 = 2131165209;
+			
+			// aapt resource value: 0x7f070011
+			public const int tableRow9 = 2131165201;
+			
+			static Id()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Id()
 			{
 			}
 		}
@@ -88,6 +210,22 @@ namespace BatteryFinal
 			}
 			
 			private String()
+			{
+			}
+		}
+		
+		public partial class Style
+		{
+			
+			// aapt resource value: 0x7f060000
+			public const int Theme_Custom = 2131099648;
+			
+			static Style()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Style()
 			{
 			}
 		}
